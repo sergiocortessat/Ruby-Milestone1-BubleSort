2de31b05c24ae0b0fa982527fd259e1b0810a5fa
Greg0109 <greg.rabago@gmail.com>
2021-02-08 13:18:21 +0100

    Add bubble_sort method

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,16 +1,22 @@
-def bubble_sort_by(array)
+def bubble_sort(array)
   i = 0
   while i < array.length - 1
-    result = yield(array[i], array[i + 1])
-    if result.positive?
-      array[i], array[i + 1] = array[i + 1], array[i]
-      i = 0
-    else
-      i += 1
-    end
+      if array[i].respond_to?(:to_str)
+          if array[i].length > array[i+1].length
+              array[i],array[i+1] = array[i+1],array[i]
+              i = 0
+          end
+      elsif array[i] > array[i+1]
+          array[i],array[i+1] = array[i+1],array[i]
+          i = 0
+      end
+      i = i + 1
   end
   array
 end
-print bubble_sort_by([14, 8, 3, 100, 3, 9, 2, 1, 0, 12, 20]) { |left, right| left - right }
+
+print bubble_sort([14,8,3,100,3,9,2,1,0,12,20])
 puts
-print bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
+print bubble_sort(["hi","hello","hey"])
+puts
+print bubble_sort([1.3,1.5,1.0,3.5,7.0,3.0])
