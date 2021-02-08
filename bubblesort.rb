f4fc3966263039544143b1fdc036f4ef87e5cb4b
Greg0109 <greg.rabago@gmail.com>
2021-02-08 11:46:32 +0100

    Fix line 4 in bubblesort. If statement not needed

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,11 +1,7 @@
 def bubblesort(array)
   i = 0
   while i < array.length - 1
-    result = if array[i].respond_to?(:to_str)
-               yield(array[i].length, array[i + 1].length)
-             else
-               yield(array[i], array[i + 1])
-             end
+    result = yield(array[i], array[i + 1])
     if result.positive?
       array[i], array[i + 1] = array[i + 1], array[i]
       i = 0
@@ -17,4 +13,4 @@ def bubblesort(array)
 end
 print bubblesort([14, 8, 3, 100, 3, 9, 2, 1, 0, 12, 20]) { |left, right| left - right }
 puts
-print bubblesort(%w[hi hello hey]) { |left, right| left - right }
+print bubblesort(%w[hi hello hey]) { |left, right| left.length - right.length }
