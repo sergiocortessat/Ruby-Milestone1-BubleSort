7a7b67795850afd07461c31c662601bc4639217e
Sergio <sbouden16@gmail.com>
2021-02-08 11:44:55 +0100

    Add fixing linters, change bubblesort line 11

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,23 +1,20 @@
 def bubblesort(array)
-    i = 0
-    while i < array.length - 1
-        if array[i].respond_to?(:to_str)
-            result = yield(array[i].length, array[i+1].length)
-        else
-            result = yield(array[i], array[i+1])
-        end
-        if result.positive?
-            array[i],array[i+1] = array[i+1],array[i]
-            array = bubblesort(array) { |left, right| left - right }
-        end
-        i = i + 1
+  i = 0
+  while i < array.length - 1
+    result = if array[i].respond_to?(:to_str)
+               yield(array[i].length, array[i + 1].length)
+             else
+               yield(array[i], array[i + 1])
+             end
+    if result.positive?
+      array[i], array[i + 1] = array[i + 1], array[i]
+      i = 0
+    else
+      i += 1
     end
-    return array
+  end
+  array
 end
-
-
-
-
-print bubblesort([14,8,3,100,3,9,2,1,0,12,20]) { |left, right| left - right }
+print bubblesort([14, 8, 3, 100, 3, 9, 2, 1, 0, 12, 20]) { |left, right| left - right }
 puts
-print bubblesort(["hi","hello","hey"]) { |left, right| left - right }
+print bubblesort(%w[hi hello hey]) { |left, right| left - right }
